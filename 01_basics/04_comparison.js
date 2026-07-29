67016c3643f902a6f319b100d4254e3de8620a68
Taran <taranpreet0048.becse25@chitkara.edu.in>
2026-07-29 11:29:03 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log(2>2)
+console.log(2<1)
+console.log(2=="2")             // check value 
+console.log(2==="2")            //check datatype also
+console.log(2!=1)
+
+console.log("2">1)
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null===0)
+console.log(null>=0)  //true         //conversion convert null to number so, null=0 
+                              // == and >= works differently
