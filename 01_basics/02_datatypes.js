6046fca8630d7f083c1e217de040a45fec7a26b7
Taran <taranpreet0048.becse25@chitkara.edu.in>
2026-07-29 09:26:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";   //treat all js code to newer version
+
+// alert(3+3)   //we are using node js not browser
+
+// console.log(3+3) console.log("taran")  // no code readability
+
+console.log(3+3)        //better readability
+console.log("trn")      //better
+
+let name="taran"         //string
+let age=19               //number
+let isLoggedIn=false     //boolean
+
+//number
+// range--> 2^53
+
+//bigint
+//string   -->" "/' '
+//boolean  -->true/false
+//null     -->standalone value , representation of empty value
+//undefined  -->value not defined
+//symbol   -->to find uniqueness
+
+//object
+
+console.log(typeof null);    //object
+console.log(typeof undefined);  //undefined  //type
+
