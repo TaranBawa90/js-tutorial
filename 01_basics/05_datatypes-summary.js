fce7e55f7914ed51acfa3d336d9a19c195ab99c5
Taran <taranpreet0048.becse25@chitkara.edu.in>
2026-07-30 17:01:35 +0000

    stack and heap,strings

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -1,5 +1,6 @@
 //primitive and non primitive datatypes on based on how they are stored in memory and how they are accessed
 
+
 //Primitive --> Call by value
 // 7 types
 // String
@@ -39,3 +40,41 @@ console.log(typeof myFunction)      //function
 //Javascript - dynamically typed
 //We don't need to declare datatype of variable along with it
 
+// ******************************Stack,heap memory***********************
+
+//Stack -- Primitive
+// Heap -- Non -Primitive
+
+//stack -- copy of value
+//heap --  reference 
+
+let name1="taran"
+
+
+//name in stack
+//userOne variable in sttack but the properties in heap , userOne gets the reference of properties  
+
+//humare pass two variable h
+//var1 and var2
+//humne var2 ke andar var 1 ki value daali
+//var 1 and var2 are primitive
+//if we change var2 to var1 will not change bcz
+//stack mein copy of value hoti h donot affect original value
+
+
+//Suppose humare pass 2 variable h nonprimitive
+//user1 and user2
+//dono object h
+//user1 and user2 dono ke variable ke aam stack mein jaayenge 
+//pr jo unki properties hongi e.g email vo heap mein jaegi
+//agr hum user1=user2
+//user1 ke andar user2 ki properties daalte h
+//user1 and user2 both will refer to same object (dono ki reference same )
+// email agr hum user2 ke andr change krenge to automatically original yaani user1 mein change hoga
+
+let user1={
+    email:"abc@gmail.com"
+}
+let user2=user1
+user2.email="xyzgmail.com"
+console.log(user1.email)
---
+++ 01_basics/06_strings.js
@@ -0,0 +1,9 @@
+const name="trn"
+const age=19
+
+// console.log(name+age+" Indian")
+
+console.log(`Hello my name is ${name} .my age is ${age}`)
+//placeholders  //better
+
+const Game=new String("pubg")   //object //with index //nut not array //its key value pair
