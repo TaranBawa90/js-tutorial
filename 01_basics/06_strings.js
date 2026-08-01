5dd564a1e31350e60bbe1985a7e47ce5942c9a66
Taran <taranpreet0048.becse25@chitkara.edu.in>
2026-08-01 10:31:43 +0000

    strings

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -6,4 +6,4 @@ const age=19
 console.log(`Hello my name is ${name} .my age is ${age}`)
 //placeholders  //better
 
-const Game=new String("pubg")   //object //with index //nut not array //its key value pair
+const Game=new String("pubg")   //object //with index //not array //its key value pair
